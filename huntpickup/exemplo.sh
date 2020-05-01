23f2185d96c862830ab22e283b7a5819e4629081
Yuri <yuri.hass@gmail.com>
2020-04-30 23:20:13 -0300

    random

--- huntpickup/exemplo.sh
+++ huntpickup/exemplo.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+#1822 1031
+#1411 1031
+
 lifeA=0
-lifeMax=3
-countres=5
-countadv=12
+lifeMax=20
+countres=300
+countadv=3600
+rdm1=0
+rdm2=0
 
 hunt()
 {
 	xmacroplay -d 60 < hunt
 	((lifeA=lifeA+1))
-	((countres=countres+1))
 	onemin
 }
 
@@ -31,10 +35,47 @@ adv()
 
 onemin()
 {
-	for i in {1..70}; do
-		echo "primeiro ciclo: $i s"
+	time=$((RANDOM%20+1))
+	((time=time+60))
+	((countres=countres+time))
+	((countadv=countadv+time))
+	for i in $(seq 1 $time); do
+		echo "$i s of $time s"
 		sleep 1
 	done
+}
+
+prof()
+{
+	((rdm1=$((RANDOM%5+1))))
+	((rdm2=$((RANDOM%5+1))))
+
+	if [ "$rdm1" -eq "$rdm2" ];
+	then
+		xmacroplay -d 60 < prof
+	fi
+}
+
+cdown()
+{
+	((rdm1=$((RANDOM%5+1))))
+	((rdm2=$((RANDOM%5+1))))
+
+	if [ "$rdm1" -eq "$rdm2" ];
+	then
+		xmacroplay -d 60 < cdown
+	fi
+}
+
+dice()
+{
+	((rdm1=$((RANDOM%10+1))))
+	((rdm2=$((RANDOM%10+1))))
+
+	if [ "$rdm1" -eq "$rdm2" ];
+	then
+		xmacroplay -d 60 < dice
+	fi
 }
 
 main()
@@ -42,7 +83,7 @@ main()
 	true=1
 	while [ $true != "0" ];
 	do
-		if [ "$countadv" -eq 12 ];
+		if [ "$countadv" -ge 3600 ];
 		then
 			heal
 			adv
@@ -50,19 +91,19 @@ main()
 			((countadv=0))
 		fi
 
-		if [ "$countres" -eq 5 ];
+		if [ "$countres" -ge 300 ];
 		then
 			pickup
-			((countadv=countadv+1))
 			((countres=0))
 		fi
 
 		if [ "$lifeA" -eq "$lifeMax" ];
 		then
 			heal
-			
 		fi
-
+		prof
+		cdown
+		dice
 		hunt
 	done
 }
